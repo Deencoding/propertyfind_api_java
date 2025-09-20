83b3b06108d2a46163a819a75872a91fff397fe7
nurudeen <nurudeenalhassan94@gmail.com>
2025-09-20 11:19:53 +0100

    first commit

---
+++ src/main/java/com/nurudeen/propertyfind/PropertyfindApplication.java
@@ -0,0 +1,13 @@
+package com.nurudeen.propertyfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyfindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyfindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nurudeen/propertyfind/PropertyfindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurudeen.propertyfind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyfindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
