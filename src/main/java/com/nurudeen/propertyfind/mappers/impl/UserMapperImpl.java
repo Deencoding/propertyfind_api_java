a0481310fa99f8fa9a19ffcd522208a37993c9eb
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-09-29 14:43:58 +0100

    added user mapper impl

--- src/main/java/com/nurudeen/propertyfind/mappers/impl/UserMapperImpl.java
+++ src/main/java/com/nurudeen/propertyfind/mappers/impl/UserMapperImpl.java
@@ -1,1 +1,19 @@
+package com.nurudeen.propertyfind.mappers.impl;
 
+import com.nurudeen.propertyfind.dto.user.UserCreateDto;
+import com.nurudeen.propertyfind.entity.UserEntity;
+import com.nurudeen.propertyfind.mappers.Mapper;
+
+public class UserMapperImpl implements Mapper<UserEntity, UserCreateDto> {
+
+
+    @Override
+    public UserCreateDto mapTo(UserEntity userEntity) {
+        return null;
+    }
+
+    @Override
+    public UserEntity mapFrom(UserCreateDto userCreateDto) {
+        return null;
+    }
+}
