748cacb88233c1a41aeb29efe372cadbdbafa8de
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-09-29 15:17:19 +0100

    finished working on user mapper impl

--- src/main/java/com/nurudeen/propertyfind/mappers/impl/UserMapperImpl.java
+++ src/main/java/com/nurudeen/propertyfind/mappers/impl/UserMapperImpl.java
@@ -3,19 +3,22 @@ package com.nurudeen.propertyfind.mappers.impl;
 import com.nurudeen.propertyfind.dto.user.UserCreateDto;
 import com.nurudeen.propertyfind.entity.UserEntity;
 import com.nurudeen.propertyfind.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
 
+@Component
 public class UserMapperImpl implements Mapper<UserEntity, UserCreateDto> {
 
     private ModelMapper modelMapper;
 
     @Override
     public UserCreateDto mapTo(UserEntity userEntity) {
-        return null;
+        return modelMapper.map(userEntity, UserCreateDto.class);
     }
 
     @Override
     public UserEntity mapFrom(UserCreateDto userCreateDto) {
-        return null;
+        return modelMapper.map(userCreateDto, UserEntity.class);
     }
 }
