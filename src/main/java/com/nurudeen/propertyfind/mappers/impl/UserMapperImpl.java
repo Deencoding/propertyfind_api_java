1f25043a84537a496fa03b4f2ad276629479403f
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-09-29 15:06:44 +0100

    added version to model mapper dependency

--- src/main/java/com/nurudeen/propertyfind/mappers/impl/UserMapperImpl.java
+++ src/main/java/com/nurudeen/propertyfind/mappers/impl/UserMapperImpl.java
@@ -4,8 +4,10 @@ import com.nurudeen.propertyfind.dto.user.UserCreateDto;
 import com.nurudeen.propertyfind.entity.UserEntity;
 import com.nurudeen.propertyfind.mappers.Mapper;
 
+
 public class UserMapperImpl implements Mapper<UserEntity, UserCreateDto> {
 
+    private ModelMapper modelMapper;
 
     @Override
     public UserCreateDto mapTo(UserEntity userEntity) {
