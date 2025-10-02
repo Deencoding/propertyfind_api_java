8a742453329e3e558e2f3ae6ae837dc58ce5cbd8
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-02 13:08:07 +0100

    added repository annotation

--- src/main/java/com/nurudeen/propertyfind/repository/UserRepository.java
+++ src/main/java/com/nurudeen/propertyfind/repository/UserRepository.java
@@ -4,10 +4,12 @@ import com.nurudeen.propertyfind.entity.UserEntity;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class UserRepository {
 
     private final JdbcTemplate jdbcTemplate;
