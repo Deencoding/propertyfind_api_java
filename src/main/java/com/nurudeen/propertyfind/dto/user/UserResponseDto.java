601ef66fd5e490d9f5b3d0f46a1c3b32251f5daa
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-02 13:54:52 +0100

    created user response dto

---
+++ src/main/java/com/nurudeen/propertyfind/dto/user/UserResponseDto.java
@@ -0,0 +1,109 @@
+package com.nurudeen.propertyfind.dto.user;
+
+
+import java.time.LocalDate;
+
+public class UserResponseDto {
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String role;
+    private LocalDate registeredDate;
+    private LocalDate updatedAt;
+
+
+    // no args constructor, needed for jpa
+
+    public  UserResponseDto() {
+
+    }
+
+    // all args constructor used service layer when mapping from an entity (User) to a DTO
+
+    public UserResponseDto(Long id, String fullName, String email, String phoneNumber,
+                           LocalDate registeredDate, LocalDate updatedAt, String role) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.registeredDate = registeredDate;
+        this.updatedAt = updatedAt;
+        this.role = role;
+    }
+
+    //  getters (because object → JSON requires reading values)
+    // Setters are optional, but usually included for flexibility.
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // to string method expose data as a human-readable string
+
+    @Override
+    public String toString() {
+        return "UserResponseDto{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", role='" + role + '\'' +
+                ", registeredDate=" + registeredDate +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
