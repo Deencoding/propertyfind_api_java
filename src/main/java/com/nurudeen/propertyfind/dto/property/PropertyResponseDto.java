5e2d8204e590acfeb7bfccd495c9ee32f6d76868
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-08 12:40:51 +0100

    added setter method to property response dto

--- src/main/java/com/nurudeen/propertyfind/dto/property/PropertyResponseDto.java
+++ src/main/java/com/nurudeen/propertyfind/dto/property/PropertyResponseDto.java
@@ -67,6 +67,10 @@ public class PropertyResponseDto {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getDescription() {
         return description;
     }
