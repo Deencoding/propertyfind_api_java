b4e8aff1724ca057d44329e9c38742fc50cc3613
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-08 12:20:05 +0100

    added set id to the create response dto

--- src/main/java/com/nurudeen/propertyfind/dto/property/PropertyCreateResponseDto.java
+++ src/main/java/com/nurudeen/propertyfind/dto/property/PropertyCreateResponseDto.java
@@ -60,6 +60,10 @@ public class PropertyCreateResponseDto {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getDescription() {
         return description;
     }
