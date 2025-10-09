0b8e7b12d029b89261669a0919d421325b426673
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-09 11:39:18 +0100

    added auth service

---
+++ src/main/java/com/nurudeen/propertyfind/service/AuthService.java
@@ -0,0 +1,32 @@
+package com.nurudeen.propertyfind.service;
+
+import com.nurudeen.propertyfind.dto.auth.LoginRequestDto;
+import com.nurudeen.propertyfind.dto.auth.LoginResponseDto;
+import com.nurudeen.propertyfind.entity.UserEntity;
+import com.nurudeen.propertyfind.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    public UserRepository userRepository;
+
+    public LoginResponseDto login(LoginRequestDto dto){
+        // find user by email
+        UserEntity user = userRepository.findByEmail(dto.getEmail())
+                .orElseThrow(()->new RuntimeException("invalid email"));
+
+        // build response
+        LoginResponseDto response = new LoginResponseDto();
+        response.setId(user.getId());
+        response.setEmail(user.getEmail());
+        response.setFullName(user.getFullName());
+        response.setRole(user.getRole());
+        response.setRegisteredDate(user.getRegisteredDate());
+
+        return response;
+
+    }
+}
