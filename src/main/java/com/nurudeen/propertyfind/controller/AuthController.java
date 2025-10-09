993851325943882c7b95d611b30dbed5fe4c35b7
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-09 11:59:43 +0100

    created auth controller

---
+++ src/main/java/com/nurudeen/propertyfind/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.nurudeen.propertyfind.controller;
+
+import com.nurudeen.propertyfind.dto.auth.LoginRequestDto;
+import com.nurudeen.propertyfind.dto.auth.LoginResponseDto;
+import com.nurudeen.propertyfind.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto dto){
+        LoginResponseDto response = authService.login(dto);
+        return ResponseEntity.ok(response);
+    }
+}
