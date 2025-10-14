ae36c832b3af81e4fb0453e413c33a66e478756f
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-14 22:06:05 +0100

    added create register controller to auth controller

--- src/main/java/com/nurudeen/propertyfind/controller/AuthController.java
+++ src/main/java/com/nurudeen/propertyfind/controller/AuthController.java
@@ -2,8 +2,12 @@ package com.nurudeen.propertyfind.controller;
 
 import com.nurudeen.propertyfind.dto.auth.LoginRequestDto;
 import com.nurudeen.propertyfind.dto.auth.LoginResponseDto;
+import com.nurudeen.propertyfind.dto.user.UserCreateDto;
+import com.nurudeen.propertyfind.dto.user.UserCreateResponseDto;
 import com.nurudeen.propertyfind.service.AuthService;
+import com.nurudeen.propertyfind.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/auth")
 public class AuthController {
 
-    @Autowired
-    private AuthService authService;
+
+    private final AuthService authService;
+    private final UserService userService;
+
+    public AuthController(AuthService authService, UserService userService) {
+        this.authService = authService;
+        this.userService = userService;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto dto){
         LoginResponseDto response = authService.login(dto);
         return ResponseEntity.ok(response);
     }
+
+    // create user
+    @PostMapping("/register")
+    public ResponseEntity<UserCreateResponseDto> createUser(@RequestBody UserCreateDto dto){
+        UserCreateResponseDto response = userService.createUser(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 }
