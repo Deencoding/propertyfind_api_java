466a95bc3e5bc4d9dce2047414db162db0634c76
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-14 22:20:46 +0100

    created my user details service

---
+++ src/main/java/com/nurudeen/propertyfind/service/MyUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.nurudeen.propertyfind.service;
+
+import com.nurudeen.propertyfind.entity.UserEntity;
+import com.nurudeen.propertyfind.repository.UserRepository;
+import com.nurudeen.propertyfind.security.CustomUserPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new CustomUserPrincipal(user);
+    }
+
+}
