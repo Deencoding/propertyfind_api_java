bc2c5b6290b3f4b417aec94e8fbe81cf3b25d8ca
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-18 06:54:15 +0100

    auth service to generate jwt

--- src/main/java/com/nurudeen/propertyfind/service/AuthService.java
+++ src/main/java/com/nurudeen/propertyfind/service/AuthService.java
@@ -4,42 +4,45 @@ import com.nurudeen.propertyfind.dto.auth.LoginRequestDto;
 import com.nurudeen.propertyfind.dto.auth.LoginResponseDto;
 import com.nurudeen.propertyfind.entity.UserEntity;
 import com.nurudeen.propertyfind.repository.UserRepository;
+import com.nurudeen.propertyfind.security.JwtService;
 import com.nurudeen.propertyfind.util.PasswordUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
-
     private final UserRepository userRepository;
     private final PasswordUtils passwordUtils;
+    private final JwtService jwtService;
 
-    public AuthService(UserRepository userRepository, PasswordUtils passwordUtils) {
+    public AuthService(UserRepository userRepository, PasswordUtils passwordUtils, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordUtils = passwordUtils;
+        this.jwtService = jwtService;
     }
 
-
-    public LoginResponseDto login(LoginRequestDto dto){
-        // find user by email
+    public LoginResponseDto login(LoginRequestDto dto) {
+        //  Find user by email
         UserEntity user = userRepository.findByEmail(dto.getEmail())
-                .orElseThrow(()->new RuntimeException("invalid email"));
+                .orElseThrow(() -> new RuntimeException("Invalid email or password"));
 
-        // Compare plain password with hashed password
+        //  Compare plain password with hashed password
         if (!passwordUtils.matches(dto.getPassword(), user.getPassword())) {
-            throw new RuntimeException("invalid password");
+            throw new RuntimeException("Invalid email or password");
         }
 
-        // build response
+        //  Generate JWT token
+        String token = jwtService.generateToken(user.getEmail(), user.getId(), user.getRole().name());
+
+        // Build response
         LoginResponseDto response = new LoginResponseDto();
         response.setId(user.getId());
         response.setEmail(user.getEmail());
         response.setFullName(user.getFullName());
         response.setRole(user.getRole());
         response.setRegisteredDate(user.getRegisteredDate());
+        response.setToken(token);
 
         return response;
-
     }
 }
