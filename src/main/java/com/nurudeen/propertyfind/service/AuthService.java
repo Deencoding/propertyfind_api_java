d2faea913dab9eed797deac5a5e06a679430a47e
Nurudeen Alhassan <87506184+Deencoding@users.noreply.github.com>
2025-10-21 17:19:15 +0100

    Implemented login with AuthenticationManager

--- src/main/java/com/nurudeen/propertyfind/service/AuthService.java
+++ src/main/java/com/nurudeen/propertyfind/service/AuthService.java
@@ -5,36 +5,37 @@ import com.nurudeen.propertyfind.dto.auth.LoginResponseDto;
 import com.nurudeen.propertyfind.entity.UserEntity;
 import com.nurudeen.propertyfind.repository.UserRepository;
 import com.nurudeen.propertyfind.security.JwtService;
-import com.nurudeen.propertyfind.util.PasswordUtils;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class AuthService {
 
+    private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
-    private final PasswordUtils passwordUtils;
     private final JwtService jwtService;
 
-    public AuthService(UserRepository userRepository, PasswordUtils passwordUtils, JwtService jwtService) {
+    public AuthService(AuthenticationManager authenticationManager, UserRepository userRepository, JwtService jwtService) {
+        this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
-        this.passwordUtils = passwordUtils;
         this.jwtService = jwtService;
     }
 
     public LoginResponseDto login(LoginRequestDto dto) {
-        //  Find user by email
-        UserEntity user = userRepository.findByEmail(dto.getEmail())
-                .orElseThrow(() -> new RuntimeException("Invalid email or password"));
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword())
+        );
 
-        //  Compare plain password with hashed password
-        if (!passwordUtils.matches(dto.getPassword(), user.getPassword())) {
-            throw new RuntimeException("Invalid email or password");
-        }
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        UserEntity user = userRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
 
-        //  Generate JWT token
         String token = jwtService.generateToken(user.getEmail(), user.getId(), user.getRole().name());
 
-        // Build response
         LoginResponseDto response = new LoginResponseDto();
         response.setId(user.getId());
         response.setEmail(user.getEmail());
